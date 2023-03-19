8fbb0415df009a1443dd6fd3bdbc5e47b2523adc
amarachigoodness74 <amarachigoodness74@gmail.com>
2023-03-19 11:59:36 +0100

    added more features

--- src/App.js
+++ src/App.js
@@ -1,34 +1,45 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function calculateWinner(squares) {
   const lines = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9],
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
     [1, 4, 7],
     [2, 5, 8],
-    [3, 6, 9],
-    [1, 5, 9],
-    [3, 5, 7],
+    [0, 4, 8],
+    [2, 4, 6],
   ];
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
+      return {
+        cells: lines[i],
+        winner: squares[a],
+      };
     }
   }
   return null;
 }
 
-function Square({ value, onSquareClick }) {
+function isGameEndedWithoutAWinner() {
+  const squares = document.getElementsByClassName("square");
+  return [...squares].filter(
+    (square) => square.textContent === ""
+  ).length > 0 ? false : true;
+}
+
+function Square({ value, id, onSquareClick }) {
   return (
-    <button className="square" onClick={onSquareClick}>
+    <button className="square" id={id} onClick={onSquareClick}>
       {value}
     </button>
   );
 }
 
-function Board({ xIsNext, squares, onPlay }) {
+function Board({ xIsNext, squares, onPlay, gameEndedWithoutAWinner }) {
+  let status;
   function handleClick(i) {
     if (calculateWinner(squares) || squares[i]) {
       return;
@@ -42,11 +53,19 @@ function Board({ xIsNext, squares, onPlay }) {
     onPlay(nextSquares);
   }
 
-  const winner = calculateWinner(squares);
-  let status;
-  if (winner) {
-    status = "Winner: " + winner;
+  const getWinner = calculateWinner(squares);
+   if (getWinner?.winner) {
+    getWinner?.cells.forEach((square) => {
+      document.getElementById(square).classList.add("win");
+    });
+    status = "Winner: " + getWinner?.winner[0];
+  } else if (!getWinner?.winner && gameEndedWithoutAWinner) {
+    status = "No Winner, start all over";
   } else {
+    const squares = document.getElementsByClassName("square");
+    [...squares].forEach((square) => {
+      square.classList.remove("win");
+    });
     status = "Next player: " + (xIsNext ? "X" : "O");
   }
 
@@ -57,9 +76,10 @@ function Board({ xIsNext, squares, onPlay }) {
         <div className="board-row" key={index}>
           {[0, 1, 2].map((value) => (
             <Square
-              key={val + index * 2 + value}
-              value={squares[val + index * 2 + value]}
-              onSquareClick={() => handleClick(val + index * 2 + value)}
+              key={val + index * 2 + value - 1}
+              id={val + index * 2 + value - 1}
+              value={squares[val + index * 2 + value - 1]}
+              onSquareClick={() => handleClick(val + index * 2 + value - 1)}
             />
           ))}
         </div>
@@ -71,8 +91,17 @@ function Board({ xIsNext, squares, onPlay }) {
 function App() {
   const [history, setHistory] = useState([Array(9).fill(null)]);
   const [isMovesInAsc, setMovesInAsc] = useState(true);
+  const [gameEndedWithoutAWinner, setGameEndedWithoutAWinner] = useState(false);
   const [currentMove, setCurrentMove] = useState(0);
   const currentSquares = history[currentMove];
+
+  useEffect(() => {
+    if (isGameEndedWithoutAWinner()) {
+      setGameEndedWithoutAWinner(true)
+    } else {
+      setGameEndedWithoutAWinner(false)
+    }
+  }, [currentMove, history]);
 
   const xIsNext = currentMove % 2 === 0;
 
@@ -102,15 +131,31 @@ function App() {
     );
   }
 
-  const movesInAsc = history.map((squares, move) => formatMoves(move));
+  const movesInAsc = history.map((_, move) => formatMoves(move));
+  const movesInDesc = function () {
+    const rows = [];
+    let historyLength = history.length - 1;
+    for (let i = historyLength; i >= 0; i--) {
+      rows.push(formatMoves(i));
+    }
+    return rows;
+  };
 
   return (
     <div className="game">
       <div className="game-board">
-        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+        <Board
+          xIsNext={xIsNext}
+          squares={currentSquares}
+          onPlay={handlePlay}
+          gameEndedWithoutAWinner={gameEndedWithoutAWinner}
+        />
       </div>
       <div className="game-info">
-        <ol>{movesInAsc}</ol>
+        <button onClick={() => setMovesInAsc(!isMovesInAsc)}>
+          Sort moves in {isMovesInAsc ? "DESC" : "ASC"}
+        </button>
+        <ol>{isMovesInAsc ? movesInAsc : movesInDesc()}</ol>
       </div>
     </div>
   );
